20150703ddb610af319ca006f3eb63c896f33c09
Juan Pablo Silva Martínez <165919824+JPSM87@users.noreply.github.com>
2024-07-29 21:15:10 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,9 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-fc1cd3b0-5112-40a8-87ba-2803a28a1a9b.containerhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
+#OTROS
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
