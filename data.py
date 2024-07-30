f6773a42bdac1a3031daf04a4dfe57db8eacc6e2
Juan Pablo Silva Martínez <165919824+JPSM87@users.noreply.github.com>
2024-07-29 21:16:11 -0400

    Update data.py

---
+++ data.py
@@ -0,0 +1,20 @@
+headers = {
+    "Content-Type": "application/json",
+    }
+headers_kit = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer jknnFApafP4awfAIFfafam2fma"
+}
+
+kits_body = {
+    "cardId": 7,
+    "name": "Mi nuevo Kit"
+   }
+user_body = {
+    "firstName": "Alejandro",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+product_ids = {
+    "ids": [1, 2, 3]
+}
